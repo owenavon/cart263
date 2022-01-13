f26f8addfd0495fd0acb3c71a1967a08ca8b5169
owenavon <89999663+owenavon@users.noreply.github.com>
2022-01-13 16:16:26 -0500

    Started JavaScript Concepts Learning Material: "2.3. First-class functions"

--- experiments/javascript-concepts/js/script.js
+++ experiments/javascript-concepts/js/script.js
@@ -4,27 +4,76 @@
 
 "use strict";
 
-const NUM_CIRCLES = 10; // Use constants when you have values that will never change. Good practice.
-let circleAlpha = 50;
-let circleSizeIncrease = 50;
-
-
-// Description of setup()
 function setup() {
   createCanvas(500, 500);
 }
 
-// Description of draw()
 function draw() {
   background(0);
 
-  circleAlpha = map(mouseX, 0, width, 10, 100);
-  circleSizeIncrease = map(mouseY, 0, height, 10, 100);
+  let config = { // Using an object to pass through properties that define parameters
+    x: 250,
+    y: 250,
+    width: 200,
+    height: 200,
+    fillColor: {
+      r: 255,
+      g: 255,
+      b: 0
+    },
+    mode: CENTER
+  };
 
-  for (let i = 0; i < NUM_CIRCLES; i++) {
-    push();
-    fill(255, circleAlpha);
-    ellipse(width/2, height/2, i * circleSizeIncrease);
-    pop();
-  }
+  drawFancyRect(config); // Passing the object through to the function
 }
+
+function drawFancyRect({ x, y, width, height, fillColor, mode }) { // Destructing the object in the function parameters to break the object apart into it's individual properties
+  push();
+  fill(fillColor.r, fillColor.g, fillColor.b);
+  rectMode (mode);
+  rect (x, y, width, height);
+  pop();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use strict";
+//
+// const NUM_CIRCLES = 10; // Use constants when you have values that will never change. Good practice.
+// let circleAlpha = 50;
+// let circleSizeIncrease = 50;
+//
+//
+// // Description of setup()
+// function setup() {
+//   createCanvas(500, 500);
+// }
+//
+// // Description of draw()
+// function draw() {
+//   background(0);
+//
+//   circleAlpha = map(mouseX, 0, width, 10, 100);
+//   circleSizeIncrease = map(mouseY, 0, height, 10, 100);
+//
+//   for (let i = 0; i < NUM_CIRCLES; i++) {
+//     push();
+//     fill(255, circleAlpha);
+//     ellipse(width/2, height/2, i * circleSizeIncrease);
+//     pop();
+//   }
+// }
