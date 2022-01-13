5d642acd7f3a902c1685cc68d991d6b08c33c188
owenavon <89999663+owenavon@users.noreply.github.com>
2022-01-13 15:56:11 -0500

    Revert "Started JavaScript Concepts Learning Material: "2.2. Object parameters""
    This reverts commit 454b6a1d8d6a5ccb3c2f9a86faafaa125e202721.

--- experiments/javascript-concepts/js/script.js
+++ experiments/javascript-concepts/js/script.js
@@ -1,30 +1,33 @@
+/**
+Title of Project
+Author Name
 
-// JavaScript Concepts Learning Material
-// Owen Avon
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
 
 "use strict";
 
-const NUM_CIRCLES = 10; // Use constants when you have values that will never change. Good practice.
-let circleAlpha = 50;
-let circleSizeIncrease = 50;
 
+/**
+Description of preload
+*/
+function preload() {
 
-// Description of setup()
-function setup() {
-  createCanvas(500, 500);
 }
 
-// Description of draw()
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
 function draw() {
-  background(0);
 
-  circleAlpha = map(mouseX, 0, width, 10, 100);
-  circleSizeIncrease = map(mouseY, 0, height, 10, 100);
-
-  for (let i = 0; i < NUM_CIRCLES; i++) {
-    push();
-    fill(255, circleAlpha);
-    ellipse(width/2, height/2, i * circleSizeIncrease);
-    pop();
-  }
 }
