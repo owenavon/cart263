635fe87380d2a66049d50a9e47bf8e644bac7da6
owenavon <89999663+owenavon@users.noreply.github.com>
2022-01-23 22:13:23 -0500

    Finished ResponsiveVoice Learning Material: "3.1. ResponsiveVoice"

--- experiments/responsive-voice/js/script.js
+++ experiments/responsive-voice/js/script.js
@@ -6,19 +6,66 @@
 
 "use strict";
 
-// Description of preload()
-function preload () {
-
-}
-
+let phrase = `Hello world!`;
+let saying = ``; // Track what is currently being said. Nothing initially.
 
 // Description of setup()
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
 }
 
 
-// Description of draw()
+// Display what is currenlty being said...
 function draw() {
+  background(255);
 
+  push();
+  textSize(32);
+  textAlign(CENTER);
+  text(saying, width / 2, height / 2);
+  pop();
 }
+
+function mousePressed() {
+  responsiveVoice.speak(phrase, "UK English Male", {
+    onstart: showSpeaking, // Calls showSpeaking function when text start.
+    onend: hideSpeaking // calls hideSpeaking function when text finish.
+  });
+}
+
+function showSpeaking() {
+  saying = phrase;
+}
+
+function hideSpeaking() {
+  saying = ``;
+}
+
+// function mousePressed() {
+//   responsiveVoice.speak(`Hello world. How are you today?`, `UK English Male`, {
+//     pitch: 0.5,
+//     rate: 0.5,
+//     volume: 1
+//   }); // speak is the method that targets the responsiveVoice library.
+// }
+//
+//
+// // Description of setup()
+// function setup() {
+//   createCanvas(500, 500);
+// }
+//
+//
+// // Description of draw()
+// function draw() {
+//   background(0);
+// }
+//
+//
+// function mousePressed() {
+//   responsiveVoice.speak(`Hello world. How are you today?`, `UK English Male`, {
+//     pitch: 0.5,
+//     rate: 0.5,
+//     volume: 1
+//   }); // speak is the method that targets the responsiveVoice library.
+// }
