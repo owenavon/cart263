a709ef69ba98fb894871782e75801a28f2a9f3fb
owenavon <89999663+owenavon@users.noreply.github.com>
2022-01-24 20:46:42 -0500

    Started slamina Learning Material: "3.3. salmina"

---
+++ experiments/slamina/js/script.js
@@ -0,0 +1,18 @@
+
+// Slamina Learning Material
+// Owen Avon
+
+// Description of project would go here
+
+"use strict";
+
+// Description of setup()
+function setup() {
+
+}
+
+
+// Description of draw()
+function draw() {
+
+}
