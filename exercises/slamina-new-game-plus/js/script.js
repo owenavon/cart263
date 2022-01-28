3dc1eeaf6bd545faa9ae9bb39e06eae2eda5a59e
owenavon <89999663+owenavon@users.noreply.github.com>
2022-01-27 22:23:33 -0500

    I have incorporated the random(); function to display a different background colour each time the simulation re-loads. I also provided various hard coded numbers variables names, to allow for better legibility in my code.

--- exercises/slamina-new-game-plus/js/script.js
+++ exercises/slamina-new-game-plus/js/script.js
@@ -7,147 +7,156 @@
 "use strict";
 
 const municipalities = [ // List of Canadian city names inside an array. List from https://github.com/dariusk/corpora/blob/master/data/geography/canadian_municipalities.json
-      "toronto",
-      "montreal",,
-      "calgary",
-      "ottawa",
-      "edmonton",
-      "mississauga",
-      "winnipeg",
-      "vancouver",
-      "brampton",
-      "hamilton",
-      "quebec city",
-      "surrey",
-      "laval",
-      "halifax",
-      "london",
-      "markham",
-      "vaughan",
-      "gatineau",
-      "longueuil",
-      "burnaby",
-      "saskatoon",
-      "kitchener",
-      "windsor",
-      "regina",
-      "richmond",
-      "richmond hill",
-      "oakville",
-      "burlington",
-      "greater sudbury",
-      "sherbrooke",
-      "oshawa",
-      "saguenay",
-      "barrie",
-      "abbotsford",
-      "st. catharines",
-      "cambridge",
-      "coquitlam",
-      "kingston",
-      "whitby",
-      "guelph",
-      "kelowna",
-      "saanich",
-      "ajax",
-      "thunder bay",
-      "terrebonne",
-      "st. john's",
-      "langley",
-      "delta",
-      "waterloo",
-      "cape breton",
-      "brantford",
-      "strathcona county",
-      "red deer",
-      "pickering",
-      "kamloops",
-      "clarington",
-      "north vancouver",
-      "milton",
-      "nanaimo",
-      "lethbridge",
-      "niagara falls",
-      "repentigny",
-      "victoria",
-      "newmarket",
-      "brossard",
-      "peterborough",
-      "chilliwack",
-      "maple ridge",
-      "sault ste. marie",
-      "kawartha lakes",
-      "sarnia",
-      "prince george",
-      "drummondville",
-      "saint john",
-      "moncton",
-      "new westminster",
-      "wood buffalo",
-      "granby",
-      "norfolk county",
-      "st. albert",
-      "medicine hat",
-      "caledon",
-      "halton hills",
-      "port coquitlam",
-      "fredericton",
-      "north bay",
-      "blainville",
-      "aurora",
-      "welland",
-      "shawinigan",
-      "belleville"
-    ];
+  "toronto",
+  "montreal",,
+  "calgary",
+  "ottawa",
+  "edmonton",
+  "mississauga",
+  "winnipeg",
+  "vancouver",
+  "brampton",
+  "hamilton",
+  "quebec city",
+  "surrey",
+  "laval",
+  "halifax",
+  "london",
+  "markham",
+  "vaughan",
+  "gatineau",
+  "longueuil",
+  "burnaby",
+  "saskatoon",
+  "kitchener",
+  "windsor",
+  "regina",
+  "richmond",
+  "richmond hill",
+  "oakville",
+  "burlington",
+  "greater sudbury",
+  "sherbrooke",
+  "oshawa",
+  "saguenay",
+  "barrie",
+  "abbotsford",
+  "saint catharines",
+  "cambridge",
+  "coquitlam",
+  "kingston",
+  "whitby",
+  "guelph",
+  "kelowna",
+  "saanich",
+  "ajax",
+  "thunder bay",
+  "terrebonne",
+  "saint johns",
+  "langley",
+  "delta",
+  "waterloo",
+  "cape breton",
+  "brantford",
+  "strathcona county",
+  "red deer",
+  "pickering",
+  "kamloops",
+  "clarington",
+  "north vancouver",
+  "milton",
+  "nanaimo",
+  "lethbridge",
+  "niagara falls",
+  "repentigny",
+  "victoria",
+  "newmarket",
+  "brossard",
+  "peterborough",
+  "chilliwack",
+  "maple ridge",
+  "sault ste. marie",
+  "kawartha lakes",
+  "sarnia",
+  "prince george",
+  "drummondville",
+  "saint john",
+  "moncton",
+  "new westminster",
+  "wood buffalo",
+  "granby",
+  "norfolk county",
+  "saint albert",
+  "medicine hat",
+  "caledon",
+  "halton hills",
+  "port coquitlam",
+  "fredericton",
+  "north bay",
+  "blainville",
+  "aurora",
+  "welland",
+  "shawinigan",
+  "belleville"
+];
 
-    let titleText = {
-      string: `Name that Canadian Municipality!`,
-      x: 960,
-      y: 270
-    };
+let titleText = {
+  string: `Name that Canadian Municipality!`,
+  x: 960,
+  y: 270
+};
 
-    let instructionText = {
-      string: `A voice says the name of a municipality backwards. You say "It must be" followed by your guess.`,
-      x: 960,
-      y: 350
-    };
+let instructionText = {
+  string: `A voice says the name of a municipality backwards. You say "It must be" followed by your guess.`,
+  x: 960,
+  y: 350
+};
 
-    let startText = {
-      string: `Click Anywhere to Begin`,
-      x: 960,
-      y: 550
-    };
+let startText = {
+  string: `Click Anywhere to Begin`,
+  x: 960,
+  y: 550
+};
 
-    let simulationText = {
-      string: `It must be...`,
-      x: 960,
-      y: 270
-    };
+let simulationText = {
+  string: `It must be...`,
+  x: 960,
+  y: 270
+};
 
-    let simulationInstructionText = {
-      string: `Clap to generate voice.`,
-      x: 960,
-      y: 810
-    };
+let simulationInstructionText = {
+  string: `Clap to generate voice.`,
+  x: 960,
+  y: 810
+};
 
-    let fontSize = {
-      small: 16, // Sets a font size of 16px.
-      medium: 36, // Sets a font size of 36px.
-      large: 96 // Sets a font size of 96px.
-    };
+let fontSize = {
+  small: 16, // Sets a font size of 16px.
+  medium: 32, // Sets a font size of 36px.
+  large: 96 // Sets a font size of 96px.
+};
 
-    let colour = {
-      white: {
-        r: 255,
-        g: 255,
-        b: 255
-      },
-    }
+let colour = {
+  white: {
+    r: 255,
+    g: 255,
+    b: 255
+  },
+  red: {
+    r: 130,
+    g: 0,
+    b: 0
+  }
+}
 
-    let currentMunicipality = ``; // Chosen random municipality will be placed inside the variable. Starts out as empty.
-    let currentAnswer = ``; // Variable that stores the value of what the user guessed. Starts out as empty.
-    let state = `landing`; // Makes the program to start in the landing state.
-    let mic = undefined;
+let currentMunicipality = ``; // Chosen random municipality will be placed inside the variable. Starts out as empty.
+let currentAnswer = ``; // Variable that stores the value of what the user guessed. Starts out as empty.
+let state = `landing`; // Makes the program to start in the landing state.
+let microphone = undefined; // undefined variable that will later be associated to a variable / parameter.
+let halfLoudness = 0.5; // Assigns a value to  microphone input loudness.
+let red = 0; // Sets intial red variable to black in colour.
+let green = 0; // Sets intial red variable to green in colour.
+let blue = 0; // Sets intial red variable to blue in colour.
 
 
 
@@ -160,7 +169,7 @@ function setup() {
 }
 
 function audioInLink() {
-  mic = new p5.AudioIn(); // Links to p5 AudioIn Library.
+  microphone = new p5.AudioIn(); // Links to p5 AudioIn Library.
 }
 
 function windowResized() {
@@ -194,7 +203,7 @@ function generateAnnyang () {
     annyang.addCommands(commands); // Tells annyang to listen to commands variable.
     annyang.start(); // Initiates speech recognition.
 
-    textSize(96); // Displays text size at 32px.
+    textSize(fontSize.large); // Displays text size at 96px.
     textStyle(BOLD); // Displays text in bold font family
     textAlign(CENTER, CENTER); // Displays text in the centre (vertically & horizontally) of the canvas.
   }
@@ -216,7 +225,7 @@ function draw() {
 }
 
 function landing() {
-  background(155); // Sets the background to black in colour.
+  background(colour.red.r, colour.red.g, colour.red.b); // Sets the background to black in colour.
 
   displayTitleText(); // Calls displayTitleText function
   displayInstructionText(); // Calls displayInstructionText function.
@@ -257,7 +266,7 @@ function displayStartText() {
 
 
 function simulation() {
-  background(0); // Sets the background to black in colour.
+  background(red, green, blue); // Sets the background to black in colour.
   displaySimulationText(); // Calls the displayStaticText function.
   displaySimulationInstructionText(); // Calls the displayAnswer function.
   displayAnswer(); // Calls the displayAnswer function.
@@ -286,10 +295,10 @@ function displaySimulationInstructionText() {
 
 function displayAnswer() {
   if (currentAnswer === currentMunicipality) { // If the user answer is the correct municipality name, then...
-    fill(0, 255, 0); // Fill the background as green in colour.
+    fill(0, 180, 0); // Fill the background as green in colour.
   }
   else { // Otherwise...
-    fill(255, 0, 0); // Fill the background as red in colour.
+    fill(180, 0, 0); // Fill the background as red in colour.
   }
   text(currentAnswer, width / 2, height / 2); // Display user's answer in the centre of the canvas.
 }
@@ -338,16 +347,19 @@ function nextQuestion() { //
 
 
 function clapToNextQuestion() {
-  let level = mic.getLevel(); // Allows
+  let microphoneLevel = microphone.getLevel(); // Allows
 
-  if (level > 0.6) { // Indicates that then the level reaches 0.6 or louder, then...
+  if (microphoneLevel > halfLoudness) { // Indicates that then the level reaches 0.6 or louder, then...
     nextQuestion(); // Call the nextQuestion function
+    red = random (0, 200);
+    green = random (0, 200);
+    blue = random (0, 200);
   }
 }
 
 // KEYPRESSED FUNCTION
 function mousePressed () { // p5 function to perform action with keyboard input.
   state = `simulation`; // Runs the "simulation" state.
-  mic.start(); // Starts microphone input.
+  microphone.start(); // Starts microphone input.
   userStartAudio(); // starts the AudioContext on a user gesture, to enable audio input in browser.
 }
