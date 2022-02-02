e82e8d4b845481608aa758fac092c48b965435a6
owenavon <89999663+owenavon@users.noreply.github.com>
2022-02-02 14:42:15 -0500

    Finished spy profile generator activity: "4.3. Spy Profile Generator"
    Completed the activity. as well as organized the code and set constants.

--- activities/03-spy-profile-generator/js/script.js
+++ activities/03-spy-profile-generator/js/script.js
@@ -1,197 +1,90 @@
 
-// Slamina Learning Material
+// Spy Profile Generator Activity
 // Owen Avon
 
-// A guessing game in which the page pronounces the name of an animal backwards and the user has to figure out what it was and say the name forwards.
+// Generates a randomized spy profile for the user, and password protects it.
 
 "use strict";
 
-const animals = [ // List of animal names inside an array. List from https://github.com/dariusk/corpora/blob/master/data/animals/common.json
-      "aardvark",
-      "alligator",
-      "alpaca",
-      "antelope",
-      "ape",
-      "armadillo",
-      "baboon",
-      "badger",
-      "bat",
-      "bear",
-      "beaver",
-      "bison",
-      "boar",
-      "buffalo",
-      "bull",
-      "camel",
-      "canary",
-      "capybara",
-      "cat",
-      "chameleon",
-      "cheetah",
-      "chimpanzee",
-      "chinchilla",
-      "chipmunk",
-      "cougar",
-      "cow",
-      "coyote",
-      "crocodile",
-      "crow",
-      "deer",
-      "dingo",
-      "dog",
-      "donkey",
-      "dromedary",
-      "elephant",
-      "elk",
-      "ewe",
-      "ferret",
-      "finch",
-      "fish",
-      "fox",
-      "frog",
-      "gazelle",
-      "gila monster",
-      "giraffe",
-      "gnu",
-      "goat",
-      "gopher",
-      "gorilla",
-      "grizzly bear",
-      "ground hog",
-      "guinea pig",
-      "hamster",
-      "hedgehog",
-      "hippopotamus",
-      "hog",
-      "horse",
-      "hyena",
-      "ibex",
-      "iguana",
-      "impala",
-      "jackal",
-      "jaguar",
-      "kangaroo",
-      "koala",
-      "lamb",
-      "lemur",
-      "leopard",
-      "lion",
-      "lizard",
-      "llama",
-      "lynx",
-      "mandrill",
-      "marmoset",
-      "mink",
-      "mole",
-      "mongoose",
-      "monkey",
-      "moose",
-      "mountain goat",
-      "mouse",
-      "mule",
-      "muskrat",
-      "mustang",
-      "mynah bird",
-      "newt",
-      "ocelot",
-      "opossum",
-      "orangutan",
-      "oryx",
-      "otter",
-      "ox",
-      "panda",
-      "panther",
-      "parakeet",
-      "parrot",
-      "pig",
-      "platypus",
-      "polar bear",
-      "porcupine",
-      "porpoise",
-      "prairie dog",
-      "puma",
-      "rabbit",
-      "raccoon",
-      "ram",
-      "rat",
-      "reindeer",
-      "reptile",
-      "rhinoceros",
-      "salamander",
-      "seal",
-      "sheep",
-      "shrew",
-      "silver fox",
-      "skunk",
-      "sloth",
-      "snake",
-      "squirrel",
-      "tapir",
-      "tiger",
-      "toad",
-      "turtle",
-      "walrus",
-      "warthog",
-      "weasel",
-      "whale",
-      "wildcat",
-      "wolf",
-      "wolverine",
-      "wombat",
-      "woodchuck",
-      "yak",
-      "zebra"
-    ];
 
-    let currentAnimal = ``; // Chosen random animal will be placed inside the variable. Starts out as empty.
-    let currentAnswer = ``; // Variable that stores the value of what the user guessed. Starts out as empty.
+const INSTRUMENT_DATA = `https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`;
+const OBJECT_DATA = `https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`;
+const TAROT_DATA = `https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`;
+const USER_PROFILE = `spy-profile-data`;
+
+
+let spyProfile = {
+  name: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  secretWeapon: `**REDACTED**`,
+  password: `**REDACTED**`
+};
+
+let instrumentData = undefined;
+let objectData = undefined;
+let tarotData = undefined;
+
+
+// Description of preload()
+function preload() {
+  instrumentData = loadJSON(INSTRUMENT_DATA); // Preloads JSON instrumentData file from URL.
+  objectData = loadJSON(OBJECT_DATA); // Preloads JSON objectData file from URL.
+  tarotData = loadJSON(TAROT_DATA); // Preloads JSON tarotData file from URL.
+}
 
 
 // Description of setup()
 function setup() {
-  createCanvas(windowWidth, windowHeight); // Sets the canvas to the users screen resolution.
+  createCanvas(windowWidth, windowHeight);
 
-  if (annyang) { // Connects annyang API.
-    let commands = { // defines command object.
-      'I think it is *animal': guessAnimal // Parameter with splat variable. This allows the user to guess the animal name.
-    };
-    annyang.addCommands(commands); // Tells annyang to listen to commands variable.
-    annyang.start(); // Initiates speech recognition.
+  let data = JSON.parse(localStorage.getItem(USER_PROFILE)); // Converts the specific key from a string to an object.
 
-    textSize(96); // Displays text size at 32px.
-    textStyle(BOLD); // Displays text in bold font family
-    textAlign(CENTER, CENTER); // Displays text in the centre (vertically & horizontally) of the canvas.
+  if (data !== null) { // If there is saved data, then place it into the spyProfile.
+
+    let password = prompt(`Agent! What is your password?!`); // Prompts the user to enter their password
+    if (password === data.password) { // If the user types in the same password as what is listed in data.password, then...
+      spyProfile.name = data.name; // Puts spyProfile name information into data name information.
+      spyProfile.alias = data.alias; // Puts spyProfile alias information into data alias information.
+      spyProfile.secretWeapon = data.secretWeapon; // Puts spyProfile secretWeapon information into data secretWeapon information.
+      spyProfile.password = data.password; // Puts spyProfile password information into data password information.
+    }
   }
+  else { // If there is not save data, then...
+    generateSpyProfile(); // Calls the generateSpyProfile function.
+  }
+}
+
+
+function generateSpyProfile() {
+  spyProfile.name = prompt(`Agent! what is your name?!`); // Whatever the user enters in the prompt will be assigned to spyProfile.name.
+  let instrument = random(instrumentData.instruments); // Chooses a random element from the "instruments" property to instrument.
+  spyProfile.alias = `The ${instrument}`; // Assigns the instrument value to spyProfile.alias.
+
+  spyProfile.secretWeapon = random(objectData.objects); // Chooses a random element from the "objects" property and assigns it to spyProfile.secretWeapon.
+
+  let card = random(tarotData.tarot_interpretations); // Chooses a random element from the tarot_interpretations object.
+  spyProfile.password = random(card.keywords); // A random keyword from the "keywords" object is assigned to spyProfile.password.
+
+  localStorage.setItem(USER_PROFILE, JSON.stringify(spyProfile)); // Saves spyProfile in the broswer, with a specfifc key
 }
 
 
 // Description of draw()
 function draw() {
-  background(0); // Sets the background to black in colour.
+  background(255);
 
-  if (currentAnswer === currentAnimal) { // If the user answer is the correct animal name, then...
-    fill(0, 255, 0); // Fill the background as green in colour.
-  }
-  else { // Otherwise...
-    fill(255, 0, 0); // Fill the background as red in colour.
-  }
-  text(currentAnswer, width / 2, height / 2); // Display user's answer in the centre of the canvas.
-}
+  let profile = `** SPY PROFILE! DO NOT DISTRIBUTE! **
 
-function mousePressed() {
-  currentAnimal = random(animals); // Fetch's random value from animals array.
-  let reverseAnimal = reverseString(currentAnimal); // Asigns reverseAnimal the result of currentAnimal.
-  responsiveVoice.speak(reverseAnimal); // reverseAnimal value is spoken aloud, via the responsiveVoice API.
-}
+Name: ${spyProfile.name}
+Alias: ${spyProfile.alias}
+Secret weapon: ${spyProfile.secretWeapon}
+Password: ${spyProfile.password}`;
 
-function guessAnimal(animal) { // Calls animal parameter, which is the word that the user guessed.
-  currentAnswer = animal.toLowerCase(); // Assign the guess inside the animal pararmter into the currentAnswer. Converts the guess toLowerCase.
-}
-
-
-function reverseString(string) { // Reverses the provided string.
-  let characters = string.split(''); // Split the string into an array of characters.
-  let reverseCharacters = characters.reverse(); // Reverse the array of characters.
-  let result = reverseCharacters.join(''); // Join the array of characters back into a string.
-  return result; // Return the result.
+  push();
+  textFont(`Courier, monosapce`);
+  textSize(24);
+  textAlign(LEFT, TOP);
+  fill(0);
+  text(profile, 100, 100);
+  pop();
 }
