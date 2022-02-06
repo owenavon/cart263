d3c606517133c9c4064c519f625de5e065ee80e1
owenavon <89999663+owenavon@users.noreply.github.com>
2022-02-06 17:16:54 -0500

    Started Bubble Popper activity: "5.4. Activity: Bubble Popper"

---
+++ activities/04-bubble-popper/js/script.js
@@ -0,0 +1,29 @@
+
+// Bubble Popper Activity
+// Owen Avon
+
+// Description goes here.
+
+"use strict";
+
+
+// PRELOAD FUNCTION
+function preload() {
+  instrumentData = loadJSON(INSTRUMENT_DATA); // Preloads JSON instrumentData file from URL.
+  objectData = loadJSON(OBJECT_DATA); // Preloads JSON objectData file from URL.
+  tarotData = loadJSON(TAROT_DATA); // Preloads JSON tarotData file from URL.
+}
+
+
+
+// SETUP FUNCTION
+function setup() {
+
+}
+
+
+
+// DRAW FUNCTION
+function draw() {
+
+}
