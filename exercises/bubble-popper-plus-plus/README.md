2ce8537ed03fa5e7cb5c882ad35f4ac6dc2c45df
owenavon <89999663+owenavon@users.noreply.github.com>
2022-02-10 23:55:41 -0500

    Completed Exercise4 "bubble-popper-plus-plus".
    This final commits adds the following:
     - sounds effects for when the user wins or loses.
    
    - Adds a custom header font.
    
    - Fixes an error with the bubble popping point system, where if the user popped the same size bubble in a row, the subsequent bubble would not provide points.

--- exercises/bubble-popper-plus-plus/README.md
+++ exercises/bubble-popper-plus-plus/README.md
@@ -3,15 +3,17 @@
 Exercise 04: Bubble Popper Plus Plus
 Owen Avon
 
-// Pop bubbles with your index finger as a pin.
+// Pop bubbles with your index finger as a pin. Achieve 25 points to win. Don't miss more than two balloons.
 
 Added functionality:
 
 ** Add loading, title, and instructions screens and maybe an “ending” to the program to make it into more of a total package
-** Make it increasingly difficult to pop the bubble over time (it could get faster? smaller?) - DONE
+** Make it increasingly difficult to pop the bubble over time (it could get faster? smaller?)
 ** Improve the audiovisual presentation of the program with more interesting visuals and sound effects
 ** Count how many bubbles the user has popped over time
-** Add different types of bubbles (maybe you shouldn’t pop some of them, maybe they all react different to being poked with the pin…)
-** Use a different ml5.js feature to represent the pin (could use the Object Detector to require the player uses a pair of scissors? Though you could represent rotation then…)
 
 Audio Credit:
+"Clapping" by "ebcrosby" | Attribution 3.0 Unported (CC BY 3.0)
+
+Font Credits:
+"Retro Bubbles" by  Max Kuivenhoven | Note from author: "It’s a fun, silly and simple typeface free for personal use."
--- exercises/bubble-popper-plus-plus/js/script.js
+++ exercises/bubble-popper-plus-plus/js/script.js
@@ -2,7 +2,7 @@
 // Bubble Popper Plus Plus Exercise
 // Owen Avon
 
-// Pop bubbles with your index finger as a pin.
+// Pop bubbles with your index finger as a pin. Achieve 25 points to win. Don't miss more than two balloons.
 // ml5.js Handpose: https://learn.ml5js.org/#/reference/handpose
 
 "use strict";
@@ -17,7 +17,7 @@ let score = 0; // Starts the score board at "0".
 let bubble = undefined; // The single bubble.
 let bubbleReset = 0; // Starts with no bubbleResets.
 
-
+let gameTitleFont; // Defines custom gameTitleFont.
 let red = 0; // Sets intial red variable to black in colour.
 let green = 0; // Sets intialgrren variable to black in colour.
 let blue = 0; // Sets intial blue variable to black in colour.
@@ -32,39 +32,45 @@ let lives = { // Defines the lives.
 
 
 let titleText = {
-  string: `Poppy McPopper`,
+  string: `BALLOON DEFLATOR`,
   x: 320,
-  y: 160
-};
-
-let instructionText = {
-  string: `Use your finger to pop the on screen ballons. Don't miss more then 2.`,
-  x: 320,
-  y: 200
+  y: 85
 };
 
 let startText = {
-  string: `"Click" anywhere to begin`,
+  string: `"Click" to begin`,
   x: 320,
-  y: 260
+  y: 240
+};
+
+let instructionText = {
+  string: `Use your index finger to pop the balloons.`,
+  x: 320,
+  y: 400
+};
+
+let instructionSubText = {
+  string: `Achieve 25 points to win. Don't miss more than two balloons.`,
+  x: 320,
+  y: 425
 };
 
 let winnerText = {
-  string: `Yahoo, you popped all the ballons! Be proud of your accomplishment.`,
+  string: `Yahoo, you popped all the balloons! Be proud of your accomplishment.`,
   x: 320,
   y: 240
 };
 
 let loserText = {
-  string: `You let too many ballons escape. You make the children cry.`,
+  string: `You let too many balloons escape. You make the children cry.`,
   x: 320,
   y: 240
 };
 
 let fontSize = {
-  small: 18, // Sets a font size of 18px.
-  medium: 24, // Sets a font size of 24px.
-  large: 36 // Sets a font size of 36px.
+  small: 16, // Sets a font size of 16px.
+  medium: 22, // Sets a font size of 22px.
+  large: 50 // Sets a font size of 48px.
 };
 
 let colour = {
@@ -78,16 +84,35 @@ let colour = {
     g: 206,
     b: 195
   },
+  red: {
+    r: 255,
+    g: 0,
+    b: 0
+  },
   green: {
     r: 0,
     g: 175,
     b: 0
   },
-  red: {
-    r: 255,
-    g: 0,
-    b: 0
+  blue: {
+    r: 0,
+    g: 50,
+    b: 100
   }
+}
+
+let gameSound = {
+  winnerSFX: undefined, // Sets winnerSFX as a variable.
+  loserSFX: undefined // Sets loserSFX as a variable.
+};
+
+
+// PRELOAD FUNCTION
+function preload () { // P5 function that loads assets prior to starting the simulation.
+  gameTitleFont = loadFont ("assets/fonts/bubble.ttf") // Preloads the custom downloaded font for efficient load times.
+
+  gameSound.winnerSFX = loadSound(`./assets/sounds/clap.wav`); // Preloads the "clap.wav" for efficient load times.
+  gameSound.loserSFX = loadSound(`./assets/sounds/cry.wav`); // Preloads the "cry.wav" for efficient load times.
 }
 
 
@@ -121,43 +146,57 @@ function draw() {
 
 
 function landing() {
-  background(50); // Sets the background to black in colour.
-
+  background(colour.blue.r, colour.blue.g, colour.blue.b); // Sets the background to blue in colour.
   displayTitleText(); // Calls displayTitleText function
-  displayInstructionText(); // Calls displayInstructionText function.
   displayStartText(); // Calls displayStartText function.
+  displayInstructionText(); // Calls displayInstructionText function.
+  displayInstructionSubText(); // Calls displaySubInstructionText function.
 }
 
 
 function displayTitleText() {
   push(); // Isolates code from using global properties.
-  textFont(`century gothic`); // Displays customFont.ttf.
+  textFont(gameTitleFont); // Displays customFont.otf.
   textSize(fontSize.large); // Displays the font size 36px.
   fill(colour.grey.r, colour.grey.g, colour.grey.b); // Displays the instructions in orange colour.
   textAlign(CENTER, CENTER); // Dictates the text alignment style.
-  text(titleText.string, titleText.x, titleText.y); // Displays the title of the game.
+  text(titleText.string, titleText.x, titleText.y); // // Displays the game header text.
   pop(); // Isolates code from using global properties.
 }
 
 
-function displayInstructionText() {
-  push(); // Isolates code from using global properties.
-  textFont(`century gothic`); // Displays customFont.ttf.
-  textSize(fontSize.small); // Displays the font size 18px.
-  fill(colour.grey.r, colour.grey.g, colour.grey.b); // Displays the instructions in orange colour.
-  textAlign(CENTER, CENTER); // Dictates the text alignment style.
-  text(instructionText.string, instructionText.x, instructionText.y); // Displays the title of the game.
-  pop(); // Isolates code from using global properties.
-}
-
-
+// Displays the instruction subtext on the landing screen.
 function displayStartText() {
   push(); // Isolates code from using global properties.
   textFont(`century gothic`); // Displays customFont.ttf.
-  textSize(fontSize.small); // Displays the font size 18px.
+  textSize(fontSize.medium); // Displays the font size 22px.
   fill(colour.grey.r, colour.grey.g, colour.grey.b); // Displays the instructions in orange colour.
   textAlign(CENTER, CENTER); // Dictates the text alignment style.
-  text(startText.string, startText.x, startText.y); // Displays the title of the game.
+  text(startText.string, startText.x, startText.y); // Displays the start game text.
+  pop(); // Isolates code from using global properties.
+}
+
+
+// Displays the header text on the landing screen.
+function displayInstructionText() {
+  push(); // Isolates code from using global properties.
+  textFont(`century gothic`); // Displays customFont.ttf.
+  textSize(fontSize.small); // Displays the font size 16px.
+  fill(colour.grey.r, colour.grey.g, colour.grey.b); // Displays the instructions in orange colour.
+  textAlign(CENTER, CENTER); // Dictates the text alignment style.
+  text(instructionText.string, instructionText.x, instructionText.y); // Displays the game instructions text.
+  pop(); // Isolates code from using global properties.
+}
+
+
+// Displays the instruction text on the landing screen.
+function displayInstructionSubText() {
+  push(); // Isolates code from using global properties.
+  textFont(`century gothic`); // Displays customFont.ttf.
+  textSize(fontSize.small); // Displays the font size 16px.
+  fill(colour.grey.r, colour.grey.g, colour.grey.b); // Displays the instructions in orange colour.
+  textAlign(CENTER, CENTER); // Dictates the text alignment style.
+  text(instructionSubText.string, instructionSubText.x, instructionSubText.y); // Displays the game instructions subtext.
   pop(); // Isolates code from using global properties.
 }
 
@@ -313,7 +352,7 @@ function bubbleCharacteristics() {
 
 // Sets the point system for each popped bubble.
 function bubblePoints() {
-  if (bubble.size >= 150 && bubble.size <= 125) {
+  if (bubble.size >= 100 && bubble.size <= 125) {
     score = score + 1; // 1 point for each large sized popped bubble.
   }
   if (bubble.size >= 50 && bubble.size <= 99) {
@@ -335,35 +374,49 @@ function displayScoreboard() { // Main code for dynamic score board.
 // Creates the scoreboard text
 function displayScoreText() {
   push(); // Isolates code from using global properties.
-  fill(colour.grey.r, colour.grey.g, colour.grey.b); // Makes the font white in colour.
+  fill(colour.grey.r, colour.grey.g, colour.grey.b); // Makes the font grey in colour.
   textFont(`century gothic`); // Displays customFont.ttf.
   textSize(fontSize.medium); // Displays the font size as 24px.
-  text(`Score:`, 50, 65); // Displays text at the top left of the canvas.
-  text(score, 130, 65); // Displays dynamic score result at the top left of the canvas.
+  text(`Score:`, 40, 65); // Displays text at the top left of the canvas.
+  text(`Hearts:`, 470, 65); // Displays text at the top left of the canvas.
+  text(score, 110, 65); // Displays dynamic score result at the top left of the canvas.
   pop(); // Isolates code from using global properties.
 }
 
 
 // Changes states upon reaching target score.
 function scoreWin() {
-  if (score >= 20) { // If the score is equal to or greater then 20...
+  if (score >= 25) { // If the score is equal to or greater then 20...
     state = `winner`; // Runs the winner state.
+    generateWinnerSFX(); // Calls the winnerSFX function.
   }
 }
 
 
 // Creates the 3 draw lives
 function displayLives() {
-  let x = lives.x; // Defines the elipses X position.
-  let y = lives.y; // Defines the elipses Y position.
-  noStroke(); // Display no stroke.
+  let healthEllipseX = lives.x; // Defines the elipses X position.
+  let healthEllipseY = lives.y; // Defines the elipses Y position.
 
   for (let i = 0; i < lives.remaining; i++) { // For loop that calls lives remaining upon running loop.
+    healthEllipseX += lives.size * 2; // Spaces the ellipses side by side on the X axis.
+    displayLivesCharacteristics(healthEllipseX, healthEllipseY);
+  }
+}
 
-    x += lives.size * 2; // Spaces the ellipses side by side on the X axis.
+function displayLivesCharacteristics(healthEllipseX, healthEllipseY) {
+  push(); // Isolates code from using global properties.
+  fill (colour.red.r, colour.red.g, colour.red.b) // Makes the ellipse grey in colour.
+  noStroke(); // Display no stroke.
+  ellipse(healthEllipseX, healthEllipseY, lives.size); // Draws ellipses.
+  pop(); // Isolates code from using global properties.
+}
 
-    ellipse(x, y, lives.size); // Draws ellipses.
-    fill (colour.grey.r, colour.grey.g, colour.grey.b)
+
+// Generates the ability for winnerSFX to be played.
+function generateWinnerSFX() {
+  if (!gameSound.winnerSFX.isPlaying()) { // If winnerSFX is not playing, then play it upon reaching a score of "20".
+    gameSound.winnerSFX.play(); // Play's winnerSFX.
   }
 }
 
@@ -387,7 +440,7 @@ function winner() {
 function winnerHeading() {
   push(); // Isolates code from using global properties.
   textFont(`century gothic`); // Displays customFont.ttf.
-  textSize(fontSize.small); // Displays the font size as 18px.
+  textSize(fontSize.small); // Displays the font size as 16px.
   fill(colour.white.r, colour.white.g, colour.white.b); // Displays the instructions in white colour.
   textAlign(CENTER, CENTER); // Dictates the text alignment style.
   text(winnerText.string, winnerText.x, winnerText.y,); // Displays the text that dictates what the user must press to start the game.
@@ -405,7 +458,7 @@ function loser() {
 function loserHeading() {
   push(); // Isolates code from using global properties.
   textFont(`century gothic`); // Displays customFont.ttf.
-  textSize(fontSize.small); // Displays the font size as 18px.
+  textSize(fontSize.small); // Displays the font size as 16px.
   fill(colour.white.r, colour.white.g, colour.white.b); // Displays the instructions in white colour.
   textAlign(CENTER, CENTER); // Dictates the text alignment style.
   text(loserText.string, loserText.x, loserText.y,); // Displays the text that dictates what the user must press to start the game.
@@ -431,10 +484,17 @@ function failedPoppingObjective() {
   }
   else if (bubbleReset > 3) { // If more then 3 bubbles get missed, then...
     state = `loser`; // Changes to the loser state.
+    generateLoserSFX(); // Calls the generateLoserSFX function.
   }
-
 }
 
+
+// Generates the ability for loserSFX to be played.
+function generateLoserSFX() {
+  if (!gameSound.loserSFX.isPlaying()) { // If loserSFX is not playing, then play it upon losing all "hearts".
+    gameSound.loserSFX.play(); // Play's loserSFX.
+  }
+}
 
 
 // MOUSEPRESSED FUNCTION
@@ -482,13 +542,13 @@ function generateBubble() {
   bubble = {
     x: random(width), // Allows the bubble to appear anywhere on the canvas's x axis.
     y: 0, // Displays the bubble at the bottom of the canvas.
-    size: 100, // Size of bubble in pixels.
+    size: 75, // Size of bubble in pixels.
     vx: 0, // No intial movement of bubble on the x axis.
-    vy: -3, // Allows the bubble to move upwards on the y axis.
+    vy: -2, // Allows the bubble to move upwards on the y axis.
     minSize: 25, // Sets a minimum bubble size.
-    maxSize: 150, // Sets a maximum bubble size.
-    minVelocity: -2, // Sets a minimum vlocity of ball movement on the y-axis.
-    maxVelocity: -4, // Sets a maximum vlocity of ball movement on the y-axis.
+    maxSize: 125, // Sets a maximum bubble size.
+    minVelocity: -1, // Sets a minimum vlocity of ball movement on the y-axis.
+    maxVelocity: -3, // Sets a maximum vlocity of ball movement on the y-axis.
     redBubble: 0, // Sets intial red variable to red in colour.
     greenBubble: 0, // Sets intial red variable to green in colour.
     blueBubble: 0 // Sets intial red variable to blue in colour.
