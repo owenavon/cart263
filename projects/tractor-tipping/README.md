1c5a716070b61553c7a471afe630feb95a61daf8
owenavon <89999663+owenavon@users.noreply.github.com>
2022-02-16 14:34:06 -0500

    Created the basis of an 8 sate system. Completed the text format on the "landing" state.

--- projects/tractor-tipping/README.md
+++ projects/tractor-tipping/README.md
@@ -3,15 +3,17 @@
 Project 01: Tractor Tipping
 Owen Avon
 
+Inspiration:
+https://www.youtube.com/watch?v=HF0s-pXOHck&ab_channel=PixarCars
+
+
 Synopsis:
+Inspired from the movie “Cars”, the user plays as "Mator" and has the task of attempting to wake sleeping tractors with the use of their voice. The player must do so cautiously, as there is a fine line between waking up the tractor and earning a point, to disturbing the authority combined figure “Frank”. The user wins if they can “tip” 3 tractors without disturbing Frank.
 
-Inspired from the movie “Cars”, the user plays as Mator and has the task of attempting to wake sleeping tractors with the use of their voice. The player must do so cautiously, as there is a fine line between waking up the tractor and earning a point, to disturbing the authority combined figure “Frank”. The user wins if they can “tip” 3 tractors without disturbing Frank.
-
-However, If Frank is disturbed, he will chase Mator along the screen for up to 30 seconds. The user who controls Mator must avoid all obstacles like fences and hay bales in order to louse Frank and win the objective.
+However, If Frank is disturbed, he will chase "Mator" along the screen for up to 30 seconds. The user who controls "Mator" must avoid all obstacles like fences and hay bales in order to louse Frank and win the objective.
 
 
 Technical functionality:
-
 ** State System
 ** Timer
 ** Responsive Voice
--- projects/tractor-tipping/js/script.js
+++ projects/tractor-tipping/js/script.js
@@ -6,9 +6,111 @@
 
 "use strict";
 
+
+let state = `landing`; // Provides the starting state. Can be "landing", "redTractor", "greenTractor", "blueTractor", "chase", "winner", "loser".
+let disneyFont; // Defines custom disneyFont.
+let titleFont; // Defines custom titleFont.
+// let gameFont; // Defines custom gameFont.
+// let timer = 5; // Set's the timer value.
+
+
+let titleText = {
+  string: `Tractor Tipping`,
+  x: 640,
+  y: 315
+};
+
+let disneyText = {
+  string: `Disney PIXAR`,
+  x: 640,
+  y: 125
+};
+
+let presentsText = {
+  string: `presents`,
+  x: 640,
+  y: 225
+};
+
+let startText = {
+  string: `Press ENTER to start tipping`,
+  x: 640,
+  y: 550
+};
+
+let winnerText = {
+  string: `see ya Frank!`,
+  x: 640,
+  y: 275
+};
+
+let loserText = {
+  string: `Hehe, Frank got ye!`,
+  x: 640,
+  y: 275
+};
+
+let fontSize = {
+  extraSmall: 16, // Sets a font size of 16px.
+  small: 18, // // Sets a font size of 18px.
+  medium: 32, // Sets a font size of 32px.
+  large: 96 // Sets a font size of 96px.
+};
+
+let colour = {
+  white: {
+    r: 255,
+    g: 255,
+    b: 255
+  },
+  grey: {
+    r: 200,
+    g: 200,
+    b: 200
+  },
+  purple: {
+    r: 50,
+    g: 20,
+    b: 50
+  },
+  orange: {
+    r: 255,
+    g: 165,
+    b: 0
+  },
+  red: {
+    r: 140,
+    g: 0,
+    b: 0
+  },
+  green: {
+    r: 0,
+    g: 140,
+    b: 0
+  },
+  blue: {
+    r: 0,
+    g: 0,
+    b: 100
+  },
+  yellow: {
+    r: 185,
+    g: 185,
+    b: 0
+  }
+};
+
+// let gameSound = {
+//   wrongSFX: undefined, // Sets wrongSFX as a variable.
+//   failSFX: undefined, // Sets failSFX as a variable.
+//   winnerSFX: undefined // Sets winnerSFX as a variable.
+// };
+
+
 // Description of preload()
 function preload () {
-
+  titleFont = loadFont ("assets/fonts/magnetob.ttf") // Preloads the custom downloaded font for efficient load times.
+  disneyFont = loadFont ("assets/fonts/disney.ttf") // Preloads the custom downloaded font for efficient load times.
 }
 
 
@@ -19,8 +121,83 @@ createCanvas(1280, 720); // Sets the canvas size to 16:9 aspect ratio.
 
 
 // Description of draw()
-function draw() {
-background(0); // Sets the background to black in colour.
+function draw() { // P5 Function that displays output on canvas.
+  if (state === `landing`) { // Indicates that when the state equates to "landing", start said state.
+    landing(); // Calls landing function.
+  }
+  else if (state === `instruction`) { // Indicates that when the state equates to "redTractor", start said state.
+    instruction(); // Calls instruction function.
+  }
+  else if (state === `redTractor`) { // Indicates that when the state equates to "redTractor", start said state.
+    simulation(); // Calls simulation function.
+  }
+  else if (state === `greenTractor`) { // Indicates that when the state equates to "greenTractor", start said state.
+    greenTractor(); // Calls greenTractor function.
+  }
+  else if (state === `blueTractor`) { // Indicates that when the state equates to "blueTractor", start said state.
+    blueTractor(); // Calls blueTractor function.
+  }
+  else if (state === `chase`) { // Indicates that when the state equates to "chase", start said state.
+    chase(); // Calls chase function.
+  }
+  else if (state === `winner`) { // Indicates that when the state equates to "winner", start said state.
+    winner(); // Calls winner function.
+  }
+  else if (state === `loser`) { // Indicates that when the state equates to "loser", start said state.
+    loser(); // Calls loser function.
+  }
+}
 
 
+// LANDING FUNCTION
+function landing() {
+  background(colour.purple.r, colour.purple.g, colour.purple.b); // Sets background to purple in colour.
+  disneyPixarText(); // Calls the disneyPixarText function.
+  subHeadingText(); // Calls the presentsText function.
+  headingText(); // Calls the headingText function.
+  startingText(); // Calls the startingText function.
+}
+
+
+function disneyPixarText() {
+  push(); // Isolates code from using global properties.
+  textFont(disneyFont); // Displays text font as disney.ttf.
+  textSize(fontSize.medium); // Displays the font size as 32px.
+  fill(colour.grey.r, colour.grey.g, colour.grey.b); // Displays the instructions in white colour.
+  textAlign(CENTER, CENTER); // Dictates the text alignment style.
+  text(disneyText.string, disneyText.x, disneyText.y); // Displays the Sub Heading.
+  pop(); // Isolates code from using global properties.
+}
+
+
+function subHeadingText() {
+  push(); // Isolates code from using global properties.
+  textFont(`courier`); // Displays text font as "calibri".
+  textSize(fontSize.extraSmall); // Displays the font size as 32px.
+  fill(colour.white.r, colour.white.g, colour.white.b); // Displays the instructions in white colour.
+  textAlign(CENTER, CENTER); // Dictates the text alignment style.
+  text(presentsText.string, presentsText.x, presentsText.y); // Displays the Sub Heading.
+  pop(); // Isolates code from using global properties.
+}
+
+
+function headingText() {
+  push(); // Isolates code from using global properties.
+  textFont(titleFont); // Displays text font as magnetob.ttf.
+  textSize(fontSize.large); // Displays the font size 46px.
+  fill(colour.yellow.r, colour.yellow.g, colour.yellow.b); // Displays the instructions in orange colour.
+  textAlign(CENTER, CENTER); // Dictates the text alignment style.
+  text(titleText.string, titleText.x, titleText.y); // Displays the title of the game.
+  pop(); // Isolates code from using global properties.
+}
+
+
+function startingText() {
+  push(); // Isolates code from using global properties.
+  textFont(`courier`); // Displays text font as "calibri".
+  textSize(fontSize.small); // Displays the font size as 32px.
+  fill(colour.white.r, colour.white.g, colour.white.b); // Displays the instructions in white colour.
+  textAlign(CENTER, CENTER); // Dictates the text alignment style.
+  text(startText.string, startText.x, startText.y); // Displays the Sub Heading.
+  pop(); // Isolates code from using global properties.
 }
