0c7766eb32f5094bed03548d28d7ff75daa37e3d
owenavon <89999663+owenavon@users.noreply.github.com>
2022-03-08 18:27:20 -0500

    Completed Raving Redactionist activity: "7.5. Activity: Raving Redactionis…
    I improved the program by assigning  static values to constants.

--- activities/06-raving-redactionist/js/script.js
+++ activities/06-raving-redactionist/js/script.js
@@ -1,9 +1,30 @@
-/**
-Raving Redactionist
-Owen Avon
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+// Raving Redactionist
+// Owen Avon
+
+// A hacker is uncensoring top secret information. You must click on the revealed information and redact as quickly as possible.
 
 "use strict";
+
+const HALF_SECOND = 500; // Assigns 500 milliseconds to HALF_SECOND constant.
+const TEN_PERCENT = 0.1; // Assigns 0.1 value to TEN_PERCENT constant.
+
+setInterval(revelation, HALF_SECOND); // Calls revelation function every 0.5 second.
+$(`.top-secret`).on(`click`, redact); // Selects top-secret class, calls click listener, and calls function redact.
+
+function redact(event) { // Defines the redact function. event is passed since the function is listening via .on
+  $(this).removeClass(`revealed`); // Removes the revealed class.
+  $(this).addClass(`redacted`); // Adds the redacted class.
+}
+
+function revelation() { // Defines the revelation function.
+  $(`.redacted`).each(attemptReveal); // Selects currently redacted text.
+}
+
+function attemptReveal() { // Defines the attemptReveal function.
+  let numberPercentage = Math.random(); // Assigns numberPercentage to random function.
+  if (numberPercentage < TEN_PERCENT) { // If the random number is less then a 10% chance, then...
+    $(this).removeClass(`redacted`); // Remove the redacted class.
+    $(this).addClass(`revealed`); // Adds the revealed class.
+  }
+}
