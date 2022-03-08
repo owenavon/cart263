e98547bb239041f4cd5fd941e54777278d49cb06
owenavon <89999663+owenavon@users.noreply.github.com>
2022-03-08 18:47:05 -0500

    Started "raving-redactionist-plus-plus" Exercise.
    I copied the content from "7.5. Activity: Raving Redactionist", added a README.md file, and modified the basic structure to prepare for this exercise.

---
+++ exercises/raving-redactionist-plus-plus/README.md
@@ -0,0 +1,19 @@
+# aving Redactionist Plus Plus
+
+Exercise 06: Raving Redactionist Plus Plus
+Owen Avon
+
+// A hacker is un-censoring top secret information. You must click on the revealed information in order for it to redact as quickly as possible. When you click, take note of the letter that is spoken aloud. Enter the keyword to jam the hacking.
+
+Added functionality:
+
+** Improve the visual presentation by working with the CSS and HTML - Modal instruction box
+** Use a fading effect to fade the redaction effect in and out (this will likely require .animate())
+** Add an ending of some kind to the experience if the user allows all the secrets to be revealed (“YOU LOST THE COLD WAR!!”) - Annyang! says a letter every time the user clicks on revealed text.
+** Custom: Implement a text input field for the user to enter the secret keyword. The Keyword will stop the hacking.
+
+Audio Credit:
+"" by "" |
+
+Font Credits:
+"" by "" |
---
+++ exercises/raving-redactionist-plus-plus/js/script.js
@@ -0,0 +1,30 @@
+
+// Raving Redactionist Plus Plus
+// Owen Avon
+
+// Click on the revealed information in order for it to redact as quickly as possible. See README.md for further information.
+
+"use strict";
+
+const HALF_SECOND = 500; // Assigns 500 milliseconds to HALF_SECOND constant.
+const TEN_PERCENT = 0.1; // Assigns 0.1 value to TEN_PERCENT constant.
+
+setInterval(revelation, HALF_SECOND); // Calls revelation function every 0.5 second.
+$(`.top-secret`).on(`click`, redact); // Selects top-secret class, calls click listener, and calls function redact.
+
+function redact(event) { // Defines the redact function. event is passed since the function is listening via .on
+  $(this).removeClass(`revealed`); // Removes the revealed class.
+  $(this).addClass(`redacted`); // Adds the redacted class.
+}
+
+function revelation() { // Defines the revelation function.
+  $(`.redacted`).each(attemptReveal); // Selects currently redacted text.
+}
+
+function attemptReveal() { // Defines the attemptReveal function.
+  let numberPercentage = Math.random(); // Assigns numberPercentage to random function.
+  if (numberPercentage < TEN_PERCENT) { // If the random number is less then a 10% chance, then...
+    $(this).removeClass(`redacted`); // Remove the redacted class.
+    $(this).addClass(`revealed`); // Adds the revealed class.
+  }
+}
