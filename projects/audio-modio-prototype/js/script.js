6e8c3e88bddfe34e86f96caaee6a426e5691b084
owenavon <89999663+owenavon@users.noreply.github.com>
2022-03-23 19:50:55 -0400

    With the use of jQuery, I implemented the fadeToggle method with the use of setInterval to create a pulsing effect. I also setup the basic structure of jQuery UI to call a dialog box. Lastly, I spent further emphasis towards the overall style of the program.

--- projects/audio-modio-prototype/js/script.js
+++ projects/audio-modio-prototype/js/script.js
@@ -10,6 +10,7 @@
   const player = document.getElementById('player');
   const startButton = document.getElementById('start');
   const stopButton = document.getElementById('stop');
+  const visualizerButton = document.getElementById('see-visualizer');
 
 
   $("#player").hide(); // Hides the player id by default.
@@ -18,9 +19,13 @@
   $("#stop").hide(); // Hides the stop button by default.
   $("#done-edit").hide(); // Hides the done-edit button by default.
   $("#see-visualizer").hide(); // Hides the see-visualizer button by default.
+  $(`#visualizer`).hide(); // Hides the visualizer content upon starting the program.
   $(".master-controls").hide(); // Hides the master editable sliders by default.
 
+
   $("#start-stop-button").text(`Press the "Start" button to record your voice.`); // Default displayed text
+
+  $(`#record-rectangle`).hide();
 
   const handleSuccess = function(stream) {
     const options = {
@@ -30,7 +35,7 @@
     const mediaRecorder = new MediaRecorder(stream, options);
 
 
-    // DATAAVAILABLE METHOD
+    // DATA_AVAILABLE METHOD
     mediaRecorder.addEventListener('dataavailable', function(e) { // dataavailable fires when mediaRecorder delivers media.
       if (e.data.size > 0) recordedChunks.push(e.data);
     });
@@ -46,24 +51,31 @@
 
       $("#download-edit").show(); // Shows the download-show button once the user says the modifications are complete.
 
-      $("#edit-recording").text(`Edit your recording`); // Default displayed text.
+      $("#edit-recording").text(`Edit your recording:`); // Default displayed text.
       $("#player").show(); // Displays the player interface upon stopping the event litener.
       $(".master-controls").show(); // Shows the master editable sliders by default.
       $("#done-edit").show(); // Displays the done-edit button.
       $("#see-visualizer").show(); // Displays the see-visualizer button.
 
+      $("#start-stop-record").hide(); // Hides the start-stop-record text.
       $("#press-play-text").text(`Press the "Play" arrow to hear your recording.`); // Dynamically changes the html text upon clicking the start button.
 
       $("#start-stop-button").hide(); // Hides the player interface upon stopping the event litener.
       $("#start-stop-record").hide(); // Hides the player interface upon stopping the event litener.
       $("#stop").hide(); // Hides the player interface upon stopping the event litener.
       $("#start").hide(); // Hides the player interface upon stopping the event litener.
+
+      setInterval(function() {
+        $(`#record-rectangle`).remove(); // Removes the recording rectangle.
+      });
+
     });
 
 
     // STOP RECORIDNG
     stopButton.addEventListener('click', function() { // Event listener that listens for button click.
       mediaRecorder.stop(); // Stops audio input recording upon event listener button.
+
     });
 
 
@@ -71,18 +83,27 @@
     startButton.addEventListener('click', function() { // Event listener that listens for button click.
       mediaRecorder.start(); // Starts audio input recording upon event listener button.
 
-      $("#start-stop-button").hide(`Press the "Start" button to record your voice.`); // Default displayed text
+      $("#start-stop-button").hide(); // Hides the start-stop-button text.
       $("#start-stop-record").text(`Press the "Stop" button to terminate the recording.`); // Dynamically changes the html text upon clicking the start button.
 
       $("#stop").show(); // Shows the stop button once the user has started the recording.
       $("#start").hide(); // Hides the start button once the user has started the recording.
 
+      setInterval(function() {
+        $(`#record-rectangle`).fadeToggle(1000);
+      });
     });
+
+    // SEE VISUALIZER
+    visualizerButton.addEventListener(`click`, function() {
+      $(`#visualizer`).dialog(); // Opens the dialogbox.
+    })
 
 
     // CROSS BROWSER
     const AudioContext = window.AudioContext || window.webkitAudioContext;
     const audioCtx = new AudioContext();
+
 
     // VOLUME
     const gainNode = audioCtx.createGain();
